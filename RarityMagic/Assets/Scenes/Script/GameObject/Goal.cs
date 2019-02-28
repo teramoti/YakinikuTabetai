c5e7263a9bff419567422a2ac4e313a2964c23a3
SatoMomoya <goodragon2158@gmail.com>
2019-02-28 14:37:02 +0900

    発表時のプロジェクトを追加しました

---
+++ RarityMagic/Assets/Scenes/Script/GameObject/Goal.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//writer name is SatoMomoya
+//ゴールした時のスクリプト
+namespace Momoya
+{
+    public class Goal : MonoBehaviour
+    {
+        //定数の宣言
+
+        //変数の宣言
+        [SerializeField]
+        private Player player; //プレイヤー
+
+        [SerializeField]
+        private int rarityCondition;        //ゴール時のレアリティ条件
+        Flag goalFlag;   //ゴールに関するフラグ
+
+        enum GoalState
+        {
+            PlayerTouch = (1 << 0),//プレイヤーがゴールに触れた
+            CanGoal     = (1 << 1),//ゴール可能 
+            CannotGoal  = (1 << 2) //ゴール不可能
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            goalFlag = GetComponent<Flag>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //もしプレイヤーがゴールに触れた場合
+            if (goalFlag.Is((uint)GoalState.PlayerTouch))
+            {
+                //ゴール条件を満たしていた場合
+                if (player.Rarity > rarityCondition - 1)
+                {
+                    //ゴール
+                    goalFlag.On((uint)GoalState.CanGoal);
+                    Debug.Log("ゴールできた");
+                }
+                else
+                {
+                    //ゴールできない
+                    goalFlag.On((uint)GoalState.CannotGoal);
+                    Debug.Log("ゴールできない");
+                }
+            }
+        }
+
+        //当たり判定(トリガー)
+        private void OnTriggerEnter(Collider other)
+        {
+            switch (other.tag)
+            {
+                case "Player": goalFlag.On((uint)GoalState.PlayerTouch); break;
+
+            }
+
+        }
+
+        //離れた判定(トリガー)
+        private void OnTriggerExit(Collider other)
+        {
+            switch (other.tag)
+            {
+                case "Player": goalFlag.Off((uint)GoalState.PlayerTouch); break;
+
+            }
+
+        }
+
+    }
+
+}
---
+++ RarityMagic/Assets/Scenes/Script/MonstersScript/Player.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//プレイヤーのスクリプト
+
+namespace Momoya
+{
+
+    public class Player : Monster
+    {
+
+        //定数の定義
+        public  const float cooltimeCount = 0.0f; //技を出したときにクールタイムを
+        //変数の定義
+        private float cooltime;                   //クールタイムをカウントする
+       
+        private Flag attackStateFlag;             //攻撃時のフラグ
+
+        private Collision hitObject;              //ヒットしたオブジェクト
+
+        //列挙型の定義
+       public enum AttackState                         //攻撃用のステート
+        {
+            CanAttack    = (1 << 0),   //攻撃可能
+            CanNotAttack = (1 << 1),   //攻撃不可能
+        }
+
+        //初期化関数
+        public override void Initialize()
+        {
+            this.attackStateFlag = GetComponent<Flag>();
+            
+            attackStateFlag.Off((uint)AttackState.CanAttack);       
+            attackStateFlag.Off((uint)AttackState.CanNotAttack);    //アタックフラグをfalseに
+        }
+
+        //Move関数
+        public override void Move()
+        {
+            //十字キーの入力をセット
+            vec.x = Input.GetAxis("Horizontal");
+            vec.y = Input.GetAxis("Vertical");
+
+            //ベクトルxが0.0じゃない場合動いている
+            if (Mathf.Abs(vec.x) != 0.0f)
+            {
+                flag.On((uint)StateFlag.Move);
+            }
+
+            //ベクトルxが0.0なら止まっている
+            if (Mathf.Abs(vec.x) == 0.0f)
+            {
+                flag.Off((uint)StateFlag.Move);
+            }
+
+            StealRarity();
+
+            GiveRarity();
+
+
+
+
+            Jump(); //ジャンプ
+
+            Debug.Log(this.rarity);
+
+        }
+        
+        //レアリティを奪う関数
+        private void StealRarity()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (GetComponentInChildren<AttackZone>().HitFlag)
+                {
+                    //当たっているオブジェクトのレアリティを下げ自分のレアリティを上げる
+                    hitObject.gameObject.GetComponent<Monster>().Rarity = hitObject.gameObject.GetComponent<Monster>().Rarity - 1;
+                    this.rarity += 1;
+                  
+
+                }
+            }
+        }
+
+        //レアリティをあげる関数
+        private void GiveRarity()
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                if (GetComponentInChildren<AttackZone>().HitFlag)
+                {
+                    //当たっているオブジェクトのレアリティを上げ自分のレアリティを下げる
+                    hitObject.gameObject.GetComponent<Monster>().Rarity = hitObject.gameObject.GetComponent<Monster>().Rarity + 1;
+                    this.rarity -= 1;
+                }
+            }
+        }
+
+        //ジャンプするための関数
+        private void Jump()
+        {
+            //スペースキーを押された時、地面についていればジャンプする
+            if (Input.GetKeyDown(KeyCode.Space) && flag.Is((uint)StateFlag.Jump))
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower);
+            }
+        }
+
+        //何が当たったのか知らせるプロパティ
+        public Collision HitObject
+        {
+            get { return hitObject; }
+            set { hitObject = value; }
+
+        }
+
+
+    }
+
+}
