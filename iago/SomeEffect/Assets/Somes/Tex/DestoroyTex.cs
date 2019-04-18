872f6cd7f13eb90a5d410a252c0f6592c4f8d621
SyunsukeGoto <gtoshun@gmail.com>
2019-04-18 16:52:48 +0900

    たくさんのエフェクト

---
+++ iago/SomeEffect/Assets/Somes/Tex/DestoroyTex.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class DestoroyTex : MonoBehaviour
+{
+    [SerializeField]
+    private VisualEffect m_VFX = null;
+    [SerializeField]
+    private SpriteRenderer m_Sprite = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            m_VFX.SendEvent("OnStart");
+            Color color = m_Sprite.color;
+            color.a = 1f;
+            m_Sprite.color = color;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Color color = m_Sprite.color;
+            color.a = 1f;
+            m_Sprite.color = color;
+        }
+    }
+}
